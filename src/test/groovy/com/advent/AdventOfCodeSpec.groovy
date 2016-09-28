bbd990da91be51d42238a6cf58a56ae1b13d5c07
temocpalma <temopalmag@gmail.com>
2016-09-27 23:28:31 -0500

    Approach for count visits and validate current instruction

--- src/test/groovy/com/advent/AdventOfCodeSpec.groovy
+++ src/test/groovy/com/advent/AdventOfCodeSpec.groovy
@@ -4,14 +4,44 @@ import spock.lang.Specification
 
 class AdventOfCodeSpec extends Specification {
 
+  AdventOfCode adventOfCode = new AdventOfCode()
 
-  def "When instruction is > should delivers presents to 2 houses"() {
+  def "When instruction is > and previous is not opposite expect true"() {
+    given:
+      String instruction = ">"
+    when:
+      Boolean result = adventOfCode.instructionIsNotOppositeToPrevious(instruction)
+    then:
+      result == true
+  }
+
+  def "When instruction is > and previous is opposite expect false"() {
+    given:
+      String instruction = ">"
+    and:
+      adventOfCode.prevInstruction = "<"
+    when:
+      Boolean result = adventOfCode.instructionIsNotOppositeToPrevious(instruction)
+    then:
+      result == false
+  }
+
+  def "When instruction is > and previous instruction is not opposite should delivers presents to 2 houses"() {
     given:"The > instruction"
-      AdventOfCode adventOfCode = new AdventOfCode()
       String instruction = ">"
     when:
       adventOfCode.processInstruction(instruction)
     then:
-      adventOfCode.visitedHouse.size == 2
+      adventOfCode.visited == 2
   }
+
+  def "When instruction is ^>v< and previous instruction is not opposite should delivers presents to 4 houses"() {
+    given:"The > instruction"
+      String instruction = "^>v<"
+    when:
+      adventOfCode.processInstruction(instruction)
+    then:
+      adventOfCode.visited == 4
+  }
+
 }
