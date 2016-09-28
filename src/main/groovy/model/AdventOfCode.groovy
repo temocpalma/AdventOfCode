c2b71db3fef1fb13a32620e3549ca5ee0f6dc23d
temocpalma <temopalmag@gmail.com>
2016-09-27 23:27:34 -0500

    Approach for count visits

--- src/main/groovy/model/AdventOfCode.groovy
+++ src/main/groovy/model/AdventOfCode.groovy
@@ -1,12 +1,21 @@
 package com.advent
 
 class AdventOfCode {
-  List visitedHouse
-  int x = 0
-  int y = 0
+  int visited = 1
+  List houses = [new House(x:0,y:0)]
+  String prevInstruction = ""
 
   void processInstruction(String instruction){
-    this.visitedHouse = [1,2]
+    instruction.each { current ->
+      if (instructionIsNotOppositeToPrevious(current)) {
+        visited++
+      }
+      prevInstruction = current
+    }
   }
 
+  Boolean instructionIsNotOppositeToPrevious(String instruction) {
+    String bothInstructions = prevInstruction+instruction
+    !(bothInstructions == "<>" || bothInstructions == "><" || bothInstructions == "v^" || bothInstructions == "^v")
+  }
 }
