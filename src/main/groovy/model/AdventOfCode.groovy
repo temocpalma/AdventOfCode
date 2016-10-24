f728f46e93065376db152a8839966e51ae597bc7
temocpalma <temopalmag@gmail.com>
2016-10-24 09:54:05 -0500

    Refactorización cambiando la ubicación por cada instrucción recibida y ajustando el proceso considerando la ubicación actual

--- src/main/groovy/model/AdventOfCode.groovy
+++ src/main/groovy/model/AdventOfCode.groovy
@@ -1,21 +1,62 @@
 package com.advent
 
 class AdventOfCode {
-  int visited = 1
-  List houses = [new House(x:0,y:0)]
+  House currentHouse = new House(x:0,y:0)
+  List houses = [currentHouse]
   String prevInstruction = ""
 
-  void processInstruction(String instruction){
-    instruction.each { current ->
-      if (instructionIsNotOppositeToPrevious(current)) {
-        visited++
+  void processInstruction(String instructions){
+    instructions.each { current ->
+      House newCurrentHouse = getNewCurrentHouseForInstruction(current)
+      if (instructionIsNotOppositeToPrevious(current) && newHouseIsNotYetVisited(newCurrentHouse)) {
+        houses << newCurrentHouse
       }
       prevInstruction = current
+      currentHouse = newCurrentHouse
     }
+  }
+
+  House getNewCurrentHouseForInstruction(String currentInstruction) {
+    int xNew = getNewXLocation(currentInstruction)
+    int yNew = getNewYLocation(currentInstruction)
+    new House(x:xNew, y:yNew)
   }
 
   Boolean instructionIsNotOppositeToPrevious(String instruction) {
     String bothInstructions = prevInstruction+instruction
     !(bothInstructions == "<>" || bothInstructions == "><" || bothInstructions == "v^" || bothInstructions == "^v")
   }
+
+  int getNewXLocation(currentInstruction) {
+    if (currentInstruction == "<") {
+      return currentHouse.x-1
+    } else if (currentInstruction == ">") {
+      return currentHouse.x+1
+    } else {
+      return currentHouse.x
+    }
+  }
+
+  int getNewYLocation(currentInstruction) {
+    if (currentInstruction == "^") {
+      return currentHouse.y-1
+    } else if (currentInstruction == "v") {
+      return currentHouse.y+1
+    } else {
+      return currentHouse.y
+    }
+  }
+
+  Boolean newHouseIsNotYetVisited(House newHouse) {
+    def exists = houses.collect {
+          if (it.x==newHouse.x && it.y==newHouse.y)
+                    it
+    }.findResults{it}.unique()
+
+    if (exists)
+      false
+    else
+      true
+  }
+
 }
