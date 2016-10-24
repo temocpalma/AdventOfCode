79ff5a97fb356eb41c2b1ad294ed46b7d6a4c68c
temocpalma <temopalmag@gmail.com>
2016-10-24 10:34:02 -0500

    Agregando clase del main

---
+++ src/main/groovy/Main.groovy
@@ -0,0 +1,14 @@
+package com.advent
+
+class Main {
+
+  static void main (String[] args) {
+    String pathInstructions = "src/main/groovy/resources/instructions.txt"
+    File instr = new File(pathInstructions)
+    String fileInstructions = instr.text
+    println "fileInstructions: ${fileInstructions}"
+    AdventOfCode advOfCode = new AdventOfCode()
+    advOfCode.processInstruction(fileInstructions)
+  }
+
+}
