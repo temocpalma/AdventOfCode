1566708a10e75fb794498a6a67cca53af03d65d8
temocpalma <temopalmag@gmail.com>
2016-10-25 13:25:12 -0500

    Agregando método para procesar las instrucciones con robo-santa

--- src/main/groovy/model/AdventOfCode.groovy
+++ src/main/groovy/model/AdventOfCode.groovy
@@ -4,20 +4,45 @@ class AdventOfCode {
   House currentHouse = new House(x:0,y:0)
   List houses = [currentHouse]
   String prevInstruction = ""
+  House locationSanta = currentHouse
+  House locationRoboSanta = currentHouse
+  Integer turn = 1//1 is Santa turn, 0 is Robo-santa turn
 
   void processInstruction(String instructions){
     instructions.each { current ->
-      House newCurrentHouse = getNewCurrentHouseForInstruction(current)
+      House newCurrentHouse = getNewCurrentHouseForInstruction(current.trim())
       if (instructionIsNotOppositeToPrevious(current) && newHouseIsNotYetVisited(newCurrentHouse)) {
         houses << newCurrentHouse
       }
       prevInstruction = current
-      currentHouse = newCurrentHouse
+      locationSanta = newCurrentHouse
     }
     println "Visited houses: ${houses.size()}"
   }
 
+  void processInstructionsWithRoboSanta(String instructions) {
+    instructions.each { current ->
+      House newCurrentHouse = getNewCurrentHouseForInstruction(current.trim())
+      if (newHouseIsNotYetVisited(newCurrentHouse)) {
+        houses << newCurrentHouse
+      }
+      relocate(newCurrentHouse)
+    }
+    println "Visited houses with Robo-Santa: ${houses.size()}"
+  }
+
+  void relocate(House newLocation) {
+    if (turn) {
+      locationSanta = newLocation
+      turn = 0
+    } else {
+      locationRoboSanta = newLocation
+      turn = 1
+    }
+  }
+
   House getNewCurrentHouseForInstruction(String currentInstruction) {
+    setCurrentHouseOfTurn()
     int xNew = getNewXLocation(currentInstruction)
     int yNew = getNewYLocation(currentInstruction)
     new House(x:xNew, y:yNew)
@@ -26,6 +51,14 @@ class AdventOfCode {
   Boolean instructionIsNotOppositeToPrevious(String instruction) {
     String bothInstructions = prevInstruction+instruction
     !(bothInstructions == "<>" || bothInstructions == "><" || bothInstructions == "v^" || bothInstructions == "^v")
+  }
+
+  void setCurrentHouseOfTurn() {
+    if (turn) {
+      currentHouse = locationSanta
+    } else {
+      currentHouse = locationRoboSanta
+    }
   }
 
   int getNewXLocation(currentInstruction) {
